295ad311cb3f729f8033ed433bc1c4320f6edd91
Pipicz István <istvan.pipicz@gmail.com>
2025-06-02 19:41:03 +0200

    refactor: Change app logo and splash screen

--- composeApp/src/androidMain/kotlin/hu/piware/bricklog/BricklogApplication.kt
+++ composeApp/src/androidMain/kotlin/hu/piware/bricklog/BricklogApplication.kt
@@ -19,7 +19,7 @@ class BricklogApplication : Application() {
         super.onCreate()
         NotifierManager.initialize(
             configuration = NotificationPlatformConfiguration.Android(
-                notificationIconResId = R.drawable.ic_launcher_foreground,
+                notificationIconResId = R.drawable.app_logo,
                 showPushNotification = true,
             )
         )
