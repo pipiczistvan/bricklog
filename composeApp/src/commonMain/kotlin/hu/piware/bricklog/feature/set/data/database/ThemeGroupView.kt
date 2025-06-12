908e26cfb57fa711ba5d8c4bb79582e399195312
Pipicz István <istvan.pipicz@gmail.com>
2025-06-12 06:22:55 +0200

    fix: Show themes crash on ios due to NullPointerException

--- composeApp/src/commonMain/kotlin/hu/piware/bricklog/feature/set/data/database/ThemeGroupView.kt
+++ composeApp/src/commonMain/kotlin/hu/piware/bricklog/feature/set/data/database/ThemeGroupView.kt
@@ -6,8 +6,8 @@ import androidx.room.DatabaseView
     viewName = "theme_group_view",
     value = """
         SELECT
-            themeGroup,
-            theme,
+            COALESCE(themeGroup, '') as themeGroup,
+            COALESCE(theme, '') as theme,
             count(*) as setCount
         FROM sets
         GROUP BY themeGroup, theme
